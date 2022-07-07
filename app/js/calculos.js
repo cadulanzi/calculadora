91312090ba98050f22bae7e06e7c5e2b9fab4781
omateusneves <omateusneves@gmail.com>
2022-07-07 20:27:01 -0300

    :lipstick: Adicionando estilo aos botões

---
+++ app/js/calculos.js
@@ -0,0 +1,57 @@
+// const btnSete = document.getElementsByClassName('.btnSete');
+
+const btnNove = document.querySelector('.btnNove');
+const btnOito = document.querySelector('.btnOito');
+const btnSete = document.querySelector('.btnSete');
+const btnSeis = document.querySelector('.btnSeis');
+const btnCinco = document.querySelector('.btnCinco');
+const btnQuatro = document.querySelector('.btnQuatro');
+const btnTres = document.querySelector('.btnTres');
+const btnDois = document.querySelector('.btnDois');
+const btnUm = document.querySelector('.btnUm');
+const btnZero = document.querySelector('.btnZero');
+
+btnNove.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 9;
+}
+)
+btnOito.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 8;
+}
+)
+btnSete.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 7;
+}
+)
+btnSeis.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 6;
+}
+)
+btnCinco.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 5;
+}
+)
+btnQuatro.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 4;
+}
+)
+btnTres.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 3;
+}
+)
+btnDois.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 2;
+}
+)
+btnUm.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 1;
+}
+)
+btnZero.addEventListener('click', () => {
+    document.querySelector("#digitado").innerHTML = 0;
+}
+)
+
+
+
+
